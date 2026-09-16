a6bdaa60161265633c67b36ae1df4191cbd211cc
kruthika <harshithashanthraju@gmail.com>
2026-09-16 21:21:46 +0530

    compoment,bean....

--- cafeteria/src/main/java/com/xworkz/cafeteria/configuration/CafeApplicationConfig.java
+++ cafeteria/src/main/java/com/xworkz/cafeteria/configuration/CafeApplicationConfig.java
@@ -2,9 +2,13 @@ package com.xworkz.cafeteria.configuration;
 
 
 import com.xworkz.cafeteria.component.Cheetha;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
+@ComponentScan("com.xworkz.cafeteria")
 public class CafeApplicationConfig {
 
     public CafeApplicationConfig(){
